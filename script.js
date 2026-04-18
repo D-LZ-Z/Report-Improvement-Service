4d2c56e870ec08d0d664fe84c97a51f249e7946a
D-LZ-Z <82749057+D-LZ-Z@users.noreply.github.com>
2026-04-18 23:35:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Landing page loaded.");
+let index = 0;
+
+setInterval(() => {
+  const slides = document.querySelector('.slides');
+  const total = slides.children.length;
+
+  index++;
+
+  if (index >= total) {
+    index = 0;
+  }
+
+  slides.style.transform = `translateX(-${index * 270}px)`;
+}, 2000);
